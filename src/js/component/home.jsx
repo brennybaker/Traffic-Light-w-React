782613c1d1a9f23f508a6cac9b9bed9c1b9838be
BreAnna Baker <124074031+brennybaker@users.noreply.github.com>
2023-04-13 18:38:26 +0000

    Very limited functionality. Need to expand on useState/implement useEFfect.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,36 @@
 import React from "react";
+import {useState} from "react"
 
 
+
+	
 //create your first component
-const Home = () => {
+
+const TrafficLight = (props) => {
+	let colors=["redlit","yellowlit","greenlit"];
+
+	const [trafficColor, setTrafficColor] = useState(null);
+
+	const redClick=()=>{
+		setTrafficColor("redlit")
+	}	
+	const yellowClick=()=>{
+		setTrafficColor ("yellowlit")
+	}
+	const greenClick=()=>{
+		setTrafficColor("greenlit");
+	}
 	return (
 			<><div className="hanger"></div>
 			<div className="bigBox">
-				<div className="redLight">
+				<div className="redLight" onClick ={redClick}>{trafficColor}
 				</div>
-				<div className="yellowLight">
+				<div className="yellowLight" onClick={yellowClick}>{trafficColor}
 				</div>
-				<div className="greenLight">
+				<div className="greenLight" onClick={greenClick}>{trafficColor}
 				</div>
 			</div></>
 	);
 };
 
-export default Home;
+export default TrafficLight;
