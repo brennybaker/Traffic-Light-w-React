176798204fa84a0dfed5bce395ff81237e8293a5
BreAnna Baker <124074031+brennybaker@users.noreply.github.com>
2024-04-25 19:25:14 +0000

    fixed render

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,39 +1,51 @@
-import React from "react";
-import {useState} from "react";
+import React, { useState } from "react";
 
 const TrafficLight = () => {
+  const [trafficColor, setTrafficColor] = useState("");
+  const [buttonStatus, setButtonStatus] = useState(false);
 
-	const [trafficColor, setTrafficColor] = useState("");
-	const handleClick = (e)=>{
-		setTimeout(()=>setTrafficColor("red"),1000);
-		setTimeout(()=>setTrafficColor("yellow"),2000);
-		setTimeout(()=>setTrafficColor("green"),3000);
-		setTimeout(()=>setTrafficColor("purple"),4000);
-		setTimeout(()=>setTrafficColor(""),5000);
-	}
+  const handleClick = (e) => {
+    setTimeout(() => setTrafficColor("red"), 1000);
+    setTimeout(() => setTrafficColor("yellow"), 2000);
+    setTimeout(() => setTrafficColor("green"), 3000);
+    setTimeout(() => setTrafficColor("purple"), 4000);
+    setTimeout(() => setTrafficColor(""), 5000);
+  };
 
-	const[buttonStatus,setButtonStatus] = useState(false);
+  const toggleButton = () => {
+    setButtonStatus(!buttonStatus);
+  };
 
-
-
-	return (
-			<><div className="hanger"></div>
-				<div className="bigBox">
-					<div className={"redLight " + (trafficColor=="red"?"redGlow":"")} onClick ={()=>setTrafficColor("red")}>
-					</div>
-					<div className={"yellowLight " + (trafficColor=="yellow"?"yellowGlow":"")}onClick={()=>setTrafficColor("yellow")}>
-					</div>
-					<div className={"greenLight " + (trafficColor=="green"?"greenGlow":"")}onClick={()=>setTrafficColor("green")}>
-					</div>
-					{buttonStatus && <div className={"purpleLight " + (trafficColor=="purple"?"purpleGlow":"")}onClick={()=>setTrafficColor("purple")}>
-					</div>}
-					<br></br>
-					<button onClick={(e)=>handleClick(e)}>Cycle</button>
-					<button onClick={toggleButton}>
-						{buttonStatus ? "Remove Purple" : "Add Purple"}
-					</button>
-				</div></>
-	);
+  return (
+    <>
+      <div className="hanger"></div>
+      <div className="bigBox">
+        <div
+          className={"redLight " + (trafficColor === "red" ? "redGlow" : "")}
+          onClick={() => setTrafficColor("red")}
+        ></div>
+        <div
+          className={"yellowLight " + (trafficColor === "yellow" ? "yellowGlow" : "")}
+          onClick={() => setTrafficColor("yellow")}
+        ></div>
+        <div
+          className={"greenLight " + (trafficColor === "green" ? "greenGlow" : "")}
+          onClick={() => setTrafficColor("green")}
+        ></div>
+        {buttonStatus && (
+          <div
+            className={"purpleLight " + (trafficColor === "purple" ? "purpleGlow" : "")}
+            onClick={() => setTrafficColor("purple")}
+          ></div>
+        )}
+        <br></br>
+        <button onClick={(e) => handleClick(e)}>Cycle</button>
+        <button onClick={toggleButton}>
+          {buttonStatus ? "Remove Purple" : "Add Purple"}
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default TrafficLight;
